ac7441e7c6188e13a0feb81f7849e2bf2122d343
Carlos Lopez <krlosaren@gmail.com>
2021-01-20 17:36:10 -0300

    Updates

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const doCache=!1,CACHE_NAME="pwa-cache";self.addEventListener("activate",(e=>{const t=[CACHE_NAME];e.waitUntil(caches.keys().then((e=>Promise.all(e.map((e=>{if(!t.includes(e))return console.log(`Deleting Cache: ${e}`),cache.delete(e)}))))))})),self.addEventListener("install",(e=>{})),self.addEventListener("fetch",(e=>{}));
